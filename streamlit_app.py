46988cc9451e078f604c05b11b533f4f75b90049
DanialAizatKamisan <danialaizat497@gmail.com>
2024-12-25 01:43:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,15 @@ import streamlit as st
 
 st.title('Final Year Project: Using Data Analytics to Identify Consumer Trends and Reduce Food Waste in Restaurants 🍽️')
 
-st.write('Hello world!')
+st.info('''
+Welcome to the **Restaurant Analytics Dashboard**!  
+This platform uses advanced data analytics to **identify consumer trends** and provide insights to help **reduce food waste** in restaurants.  
+
+**Features:**
+- View trends in consumer preferences.
+- Explore key metrics related to unit sales and waste reduction.
+- Predict future trends using machine learning models.
+
+Together, we aim to promote sustainability and improve decision-making in the restaurant industry.
+''')
+
